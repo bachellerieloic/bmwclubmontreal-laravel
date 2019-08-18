7d12d624b50f89923d61ca7603c464840def258e
bachellerieloic <loicbachellerie@me.com>
2019-08-18 16:04:57 -0400

    Landing page creation

--- resources/assets/js/views/pages/landing.vue
+++ resources/assets/js/views/pages/landing.vue
@@ -9,11 +9,6 @@
                 <div class="collapse navbar-collapse" id="navbar-default">
                     <div class="navbar-collapse-header">
                         <div class="row">
-                            <div class="col-6 collapse-brand">
-                                <a href="index.html">
-                                    <img src="assets/img/brand/blue.png">
-                                </a>
-                            </div>
                             <div class="col-6 collapse-close">
                                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
                                     <span></span>
