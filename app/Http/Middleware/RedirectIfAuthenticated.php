e02faba795db0ed2549c652f2e45f8230683f392
bachellerieloic <loicbachellerie@me.com>
2019-10-06 20:32:57 -0400

    landing page localisation

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,23 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+//        if (Auth::guard($guard)->check()) {
+//            return redirect('/home');
+//        }
+
+        $allow_origin = [
+            'http://localhost',
+            'http://localhost:3000',
+            'http://localhost:8000',
+            'http://localhost:8080',
+        ];
+
+        if(isset($_SERVER['HTTP_ORIGIN']) &&
+            in_array($_SERVER['HTTP_ORIGIN'], $allow_origin)) {
+            header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Secret');
+            header('Access-Control-Max-Age: 120');
         }
 
         return $next($request);
